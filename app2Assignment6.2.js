f8912c0db8211d1f8c198560de56b2f5606c32fd
Monicadorthy <monicadorthy@gmail.com>
2017-01-24 14:14:29 +0530

    SixthGibhub Commit
    SixthGibhub Commit

---
+++ app2Assignment6.2.js
@@ -0,0 +1,15 @@
+//var Greatestnum = Math.max(100,150,55);
+//console.log("The greatest number among 3 numbers is:" +Greatestnum);
+var a=600;
+var b=150;
+var c=555;
+if(a>b){
+    console.log("The greatest number among 3 numbers is:" +a);
+}else if(a<b){
+console.log("The greatest number among 3 numbers is:" +b);
+}
+else if(c>a){
+   console.log("The greatest number among 3 numbers is:" +c); 
+}
+
+
